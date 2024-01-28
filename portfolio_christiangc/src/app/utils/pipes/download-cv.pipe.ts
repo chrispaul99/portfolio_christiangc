fbe24ff6b8e68bc4a41f2c571b7f1b0efddfb516
Christian Gunopatin <c.paul@lkbitronic.com>
2024-01-29 00:40:19 +0100

    fix cv

--- portfolio_christiangc/src/app/utils/pipes/download-cv.pipe.ts
+++ portfolio_christiangc/src/app/utils/pipes/download-cv.pipe.ts
@@ -7,7 +7,7 @@ export class DownloadCVPipe implements PipeTransform {
 
   transform(lang:string): string {
     switch(lang){
-      case "es": return "https://drive.google.com/uc?export=download&id=122DQXYJi7sW1koCMCn4ZkpdSSj0Kcmac";
+      case "es": return "https://drive.google.com/uc?export=download&id=1dTxnb_tIc0d5dohhc_GvxKzgDiLwBiRS";
       case "en": return "https://drive.google.com/uc?export=download&id=1uUajQifwPvcpGjateOkkOMK2MpykKHMQ";
       default:return "";
     }
--- portfolio_christiangc/src/app/utils/pipes/view-cv.pipe.ts
+++ portfolio_christiangc/src/app/utils/pipes/view-cv.pipe.ts
@@ -7,7 +7,7 @@ export class ViewCvPipe implements PipeTransform {
 
   transform(lang:string): string {
     switch(lang){
-      case "es": return "https://drive.google.com/file/d/122DQXYJi7sW1koCMCn4ZkpdSSj0Kcmac/preview";
+      case "es": return "https://drive.google.com/file/d/1dTxnb_tIc0d5dohhc_GvxKzgDiLwBiRS/preview";
       case "en": return "https://drive.google.com/file/d/1uUajQifwPvcpGjateOkkOMK2MpykKHMQ/preview";
       default:return "";
     }
